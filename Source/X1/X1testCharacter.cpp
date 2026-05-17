7dae1913268ed11f2ee75f7064266ea461315c38
Ajax-Alpha <2722393697@qq.com>
2026-05-17 20:25:13 +0800

    角色移动
    实现了角色移动的功能

--- Source/X1/X1testCharacter.cpp
+++ Source/X1/X1testCharacter.cpp
@@ -16,6 +16,20 @@ void AX1testCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	check(GEngine != nullptr);
+
+	// Get the player controller for this character
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		// Get the enhanced input local player subsystem and add a new input mapping context to it
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(FirstPersonContext, 0);
+		}
+	}
+	// Display a debug message for five seconds. 
+	// The -1 "Key" value argument prevents the message from being updated or refreshed.
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("We are using X1Character."));
 }
 
 // Called every frame
@@ -29,6 +43,29 @@ void AX1testCharacter::Tick(float DeltaTime)
 void AX1testCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		// Bind Movement Actions
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AX1testCharacter::Move);
 
+		// Bind Jump Actions
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ACharacter::Jump);
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
+	}
 }
 
+void AX1testCharacter::Move(const FInputActionValue& Value)
+{
+	
+	const FVector2D MovementValue = Value.Get<FVector2D>();
+
+	if (Controller)
+	{
+		const FVector Right = GetActorRightVector();
+		AddMovementInput(Right, MovementValue.X);
+
+		const FVector Forward = GetActorForwardVector();
+		AddMovementInput(Forward, MovementValue.Y);
+	}
+}
